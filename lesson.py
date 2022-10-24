841d303ebdf4fd13285e4fd4f5b3c77d086cd50d
moneer <moneer@yandex.ru>
2022-10-24 23:33:58 +0300

    first lesson practice

---
+++ lesson.py
@@ -0,0 +1,51 @@
+# Задача №1
+# a = int(input('Enter first number: '))
+# b = int(input('Enter second number: '))
+# if a == b ** 2 or b == a ** 2:
+#     print('yes!')
+# else:
+#     print('NO!')
+
+# **********************************************************************
+
+# Задача №2
+# some_list = map(int, input('Insert numbers by space: ').split())
+# max_num = max(some_list)
+# print(max_num)
+# /////////////////////////////////////
+# number = int(input())
+# max = number
+# for i in range(0, 4):
+#     number = int(input())
+#     if number > max:
+#         max = number
+# print(max)
+# ///////////////////////////
+# some_list = []
+# for i in range(5):
+#     number = int(input())
+#     some_list.append(number)
+# maxx = some_list[0]
+# for el in some_list:
+#     if el > maxx:
+#         maxx = el
+# print(maxx)
+# ///////////////////////////////
+# print(max(map(int, input().split())))
+
+# *****************************************************
+
+# Задача №3
+# a = int(input())
+# for i in range(-a, a+1):
+#     print(i, end=', ')
+# ///////////////////////////////////////
+# num = input()
+# if '.' not in num:
+#     print('No')
+# else:
+#     n = (float(num) * 10) % 10
+#     print(int(n))
+# *********************************************************
+
+# Задача№4
